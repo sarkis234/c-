bc14d154b13f735b74e63de775c9d60ba86b2a24
sarkis234 <sarkis95283@gmail.com>
2025-04-14 21:18:33 +0300

    14.04.25

--- Program.cs
+++ Program.cs
@@ -4,175 +4,188 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace _14._04._25
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class Dima
         {
-            //Console.Read();
-            //Console.Write("Hello");
-            //Console.WriteLine("World");
-            //Console.ReadLine();
-
-            //int a = Convert.ToInt32(Console.ReadLine());
-            //int b = Convert.ToInt32(Console.ReadLine());
-            //int c = Convert.ToInt32(Console.ReadLine());
-            //int d = Convert.ToInt32(Console.ReadLine());
-            //int f = Convert.ToInt32(Console.ReadLine());
-            //int sum = a + b + c + d + f;
-            //int mult = a * b * c * d * f;
-            //Console.WriteLine("summ: " + sum.ToString());
-            //int max = a;
-            //int min = a;
-            //if (max < b)
-            //{
-            //    max = b;
-            //}
-            //if (max < c)
-            //{
-            //    max = c;
-            //}
-            //if (max < d)
-            //{
-            //    max = d;
-            //}
-            //if (max < f)
-            //{
-            //    max = f;
-            //}
-            //if (min > b)
-            //{
-            //    min = b;
-            //}
-            //if (min > c)
-            //{
-            //    min = c;
-            //}
-            //if (min > d)
-            //{
-            //    min = d;
-            //}
-            //if (min > f)
-            //{
-            //    min = f;
-            //}
-            //Console.WriteLine("max num: " + max.ToString());
-            //Console.WriteLine("min num: " + min.ToString());
-            //Console.WriteLine("multiply: " + mult.ToString());
-
-            //int a = Convert.ToInt32(Console.ReadLine());
-            //int b = 0;
-            //int c = 1;
-            //Console.Write(b.ToString() + ", " + c.ToString() + ", ");
-            //for (int i = 0; i < a; i++)
-            //{
-            //    int sum = b + c;
-            //    if (sum > a) break;
-            //    b = c;
-            //    c = sum;
-            //    Console.Write(sum.ToString() + ", ");
-            //}
-
-            //int a = Convert.ToInt32(Console.ReadLine());
-            //int b = Convert.ToInt32(Console.ReadLine());
-            //for (int i = a; i <= b; i++)
-            //{
-            //    for (int j = 0; j < i; j++)
-            //    {
-            //        int c = i + j;
-            //        Console.Write(c.ToString() + " ");
-            //    }
-            //    Console.Write("\n");
-
-            //}
-
-            //int a = Convert.ToInt32(Console.ReadLine());
-            //char s = Convert.ToChar(Console.ReadLine());
-            //Console.Write("Enter direction(0 - horizontal, 1 - vertical): ");
-            //int dir = Convert.ToInt32(Console.ReadLine());
-            //if (dir == 0)
-            //{
-            //    for (int i = 0; i < a; i++)
-            //    {
-            //        Console.Write(s);
-            //    }
-            //    Console.Write("\n");
-            //}
-            //else if (dir == 1)
-            //{
-            //    for (int i = 0; i < a; i++)
-            //    {
-            //        Console.WriteLine(s);
-            //    }
-            //}
-
-            //int[] a = new int[4]; // static
-            //int[] b = {1, 4, 6};
-            //foreach(int i in b)
-            //{
-            //    Console.WriteLine(i);
-            //}
-            //List<int> name = new List<int>(); // dinamic
-            //int[] name_l = name.ToArray();
-
-            //int[] a = { 1, 6, 4, 5, 4, 34, 34, 3, 23, 10, 1, 1, };
-            //int count = 0;
-            //Console.Write("Even numbers: ");
-            //foreach (int i in a)
-            //{
-            //    if (i % 2 == 0)
-            //    {
-            //        Console.Write(i.ToString() + " ");
-            //        count++;
-            //    }
-            //}
-            //Console.WriteLine("Even numbers amount: " + count.ToString());
-            //count = 0;
-            //Console.Write("Odd numbers: ");
-            //foreach (int i in a)
-            //{
-            //    if (i % 2 == 1)
-            //    {
-            //        Console.Write(i.ToString() + " ");
-            //        count++;
-            //    }
-            //}
-            //Console.WriteLine("Odd numbers amount: " + count.ToString());
-            //count = 0;
-            //Console.Write("Uniq numbers: ");
-            //int uniqCount = 0;
-            //foreach (int i in a)
-            //{
-            //    foreach(int j in a)
-            //    {
-            //        if (i == j)
-            //        {
-            //            uniqCount++;
-            //        }
-            //    }
-            //    if (uniqCount == 1)
-            //    {
-            //        Console.Write(i.ToString() + " ");
-            //        count++;
-            //    }
-            //    uniqCount = 0;
-            //}
-            //Console.WriteLine("Uniq numbers amount: " + count.ToString());
-
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            int c = Convert.ToInt32(Console.ReadLine());
-            int count = 0;
-            int[] arr = { 1, 6, 4, 5, 4, 34, 34, 3, 1, 6, 4, 23, 10, 1, 6, 4};
-            for(int i = 0; i < arr.Length - 2; i++)
+            private string name;
+            private string country;
+            private int population;
+            private int phoneCode;
+            private string districts;
+            public void name1(string a)
             {
-                if (arr[i] == a && arr[i + 1] == b && arr[i + 2] == c)
+                this.name = a;
+            }
+            public void country1(string a)
+            {
+                this.country = a;
+            }
+            public void population1(int a)
+            {
+                this.population = a;
+            }
+            public void phoneCode1(int a)
+            {
+                this.phoneCode = a;
+            }
+            public void districts1(string a)
+            {
+                this.districts = a;
+            }
+            public void writer()
+            {
+                Console.WriteLine($"Город {name}");
+                Console.WriteLine($"Страна {country}");
+                Console.WriteLine($"Население {population}");
+                Console.WriteLine($"Телефонный код {phoneCode}");
+                Console.WriteLine($"Район {districts}");
+            }
+
+        }
+        static int[] func3(int[] arr)
+        {
+            int[] arr2 = arr;
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length - 1; j++)
                 {
-                    count++;
+                    if (arr2[j] > arr2[j + 1])
+                    {
+                        int b = arr2[j];
+                        arr2[j] = arr2[j + 1];
+                        arr2[j + 1] = b;
+                    }
                 }
             }
-            Console.WriteLine(count);
+            return arr2;
         }
+        static int func1(int a, int b)
+        {
+            int mult = 1;
+            for(int i = a; i <= b; i++)
+            {
+                mult *= i;
+            }
+            return mult;
+        }
+
+        static bool func2(int a)
+        {
+            int b = 0;
+            int c = 1;
+            if (a == b || a == c) return true;
+            while (true)
+            {
+                int a2 = b;
+                b = b + c;
+                c = a2;
+                if (a == b)
+                {
+                    return true;
+                }
+                if (b > a)
+                {
+                    return false;
+                }
+            }
+        }
+        static bool func2n2(int a)
+        {
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(func1(1, 5));
+            //Console.WriteLine(func2(10));
+            //Console.WriteLine(func2n2(5));
+
+            //int[] arr = { 1, 234, 342, 11, 32 };
+            //arr = func3(arr);
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            //int[,] a = { { 1, 8, 11 }, { 24, 0, 6 } };
+            //int row = a.GetLength(0);
+            //int column = a.GetLength(1);
+            //int min = a[0, 0];
+            //int max = a[0, 0];
+            //for (int i = 0; i < row; i++)
+            //{
+            //    for (int j = 0; j < column; j++)
+            //    {
+            //        if (a[i, j] < min) min = a[i, j];
+            //        if (a[i, j] > max) max = a[i, j];
+            //    }
+            //}
+            //Console.WriteLine(max);
+            //Console.WriteLine(min);
+
+            //string b = Console.ReadLine();
+            //int count = 1;
+            //for (int i = 0; i < b.Length; i++)
+            //{
+            //    if (b[i] == ' ') count++;
+            //}
+            //Console.WriteLine(count);
+
+            //string c = Console.ReadLine();
+            //int count = 0;
+            //c = c.ToLower();
+            //for (int i = 0; i < c.Length; i++)
+            //{
+            //    if (c[i] == 'a' || c[i] == 'e' || c[i] == 'i' || c[i] == 'o' || c[i] == 'u' || c[i] == 'y')
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //string c = (Console.ReadLine()).ToLower();
+            //string d = Console.ReadLine().ToLower();
+            //int count = 0;
+            //for (int i= 0; i < c.Length; i++)
+            //{
+            //    bool flag = false;
+            //    if (c[i] == d[0])
+            //    {
+            //        for (int j = 0; j < d.Length; j++)
+            //        {
+            //            if (i + j > c.Length)
+            //            {
+            //                flag = false;
+            //                break;
+            //            }
+            //            if (c[i + j] != d[j])
+            //            {
+            //                flag = false;
+            //                break;
+            //            }
+            //            else flag = true;
+            //        }
+            //    }
+            //    if (flag) count++;
+            //}
+            //Console.WriteLine(count);
+
+            Dima a = new Dima();
+            a.name1("Москва");
+            a.country1("Россия");
+            a.population1(121312);
+            a.phoneCode1(1010);
+            a.districts1("Centr");
+            a.writer();
+        }
+
+
     }
+
 }
