f9a3eee11bc52fc63b4dce1a57a0207077f1616c
sarkis234 <sarkis95283@gmail.com>
2025-09-10 21:09:05 +0300

    Add files via upload

--- 08.09.25/08.09.25/Form1.Designer.cs
+++ 08.09.25/08.09.25/Form1.Designer.cs
@@ -31,21 +31,16 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.Login = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Password = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.FileBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Login,
-            this.Password,
-            this.Name,
-            this.Age});
             this.dataGridView1.Location = new System.Drawing.Point(173, 100);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(443, 150);
@@ -69,39 +64,64 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "delete user";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Login
+            // textBox1
             // 
-            this.Login.HeaderText = "Login";
-            this.Login.Name = "Login";
+            this.textBox1.Location = new System.Drawing.Point(516, 349);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
             // 
-            // Password
+            // label1
             // 
-            this.Password.HeaderText = "Password";
-            this.Password.Name = "Password";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(526, 332);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "enter login";
+            this.label1.Visible = false;
             // 
-            // Name
+            // button3
             // 
-            this.Name.HeaderText = "Name";
-            this.Name.Name = "Name";
+            this.button3.Location = new System.Drawing.Point(529, 375);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "ok";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // Age
+            // FileBtn
             // 
-            this.Age.HeaderText = "Age";
-            this.Age.Name = "Age";
+            this.FileBtn.Location = new System.Drawing.Point(697, 301);
+            this.FileBtn.Name = "FileBtn";
+            this.FileBtn.Size = new System.Drawing.Size(75, 23);
+            this.FileBtn.TabIndex = 6;
+            this.FileBtn.Text = "button4";
+            this.FileBtn.UseVisualStyleBackColor = true;
+            this.FileBtn.Click += new System.EventHandler(this.FileBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FileBtn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            //this.Name = "Form1";
+            this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -110,10 +130,10 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Login;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Password;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button FileBtn;
     }
 }
 
--- 08.09.25/08.09.25/Form1.cs
+++ 08.09.25/08.09.25/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace _08._09._25
 {
     public partial class Form1 : Form
     {
-        public List<User> users = new List<User>();
+        public BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
@@ -21,13 +22,60 @@ namespace _08._09._25
         private void printUsersInfo(User user)
         {
             users.Add(user);
-            dataGridView1.DataSource = users;
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\Взрослая академия\\source\\repos\\08.09.25\\08.09.25\\users.csv", true))
+            {
+                writer.WriteLine($"{user.Login};{user.Password};{user.Name};{user.Age}");
+            }
         }
         private void button1_Click(object sender, EventArgs e)
         {
             Form2 newForm = new Form2();
             newForm.addNewUser += printUsersInfo;
             newForm.Show();
+            
         }
-    }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            textBox1.Visible = true;
+            button3.Visible = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (var item in users)
+            {
+                if (item.Login == textBox1.Text)
+                {
+                    users.Remove(item);
+                    break;
+                }
+            }
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\Взрослая академия\\source\\repos\\08.09.25\\08.09.25\\users.csv", false)){}
+            foreach (var item in users)
+            {
+                using (StreamWriter writer = new StreamWriter("C:\\Users\\Взрослая академия\\source\\repos\\08.09.25\\08.09.25\\users.csv", true))
+                {
+                    writer.WriteLine($"{item.Login};{item.Password};{item.Name};{item.Age}");
+                }
+            }
+            label1.Visible = false;
+            textBox1.Visible = false;
+            button3.Visible = false;
+        }
+
+        private void FileBtn_Click(object sender, EventArgs e)
+        {
+            //    using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+            //        if (openFileDialog.ShowDialog() == DialogResult.OK)
+            //        {
+            //            openFileDialog.Filter = 
+            //            string path = openFileDialog.FileName;
+
+            //        }
+            //    }
+        }
+}
 }
