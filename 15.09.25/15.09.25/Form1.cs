5a8672bc58126c755d2f1644f71601ad4e6406c7
sarkis234 <sarkis95283@gmail.com>
2025-09-15 21:12:28 +0300

    Add files via upload

---
+++ 15.09.25/15.09.25/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _15._09._25
+{
+    public partial class Form1 : Form
+    {
+        logger l1 = new logger();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void readFile(string path)
+        {
+            try
+            {
+                File.OpenRead(path);
+            }
+            catch (Exception ex)
+            {
+                l1.logging(ex.Message);
+            }
+            finally
+            {
+                l1.logging("POSTOL");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "";
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+            //if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    path = saveFileDialog.FileName;
+            //}
+            //using (StreamWriter writer = new StreamWriter(path, false))
+            //{
+            //    writer.WriteLine(textBox1.Text);
+            //}
+            readFile(path);
+        }
+    }
+}
---
+++ 15.09.25/15.09.25/logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+namespace _15._09._25
+{
+    public class logger
+    {
+        string path = "log.txt";
+        DateTime dateTime = DateTime.Now;
+        bool isActive = false;
+
+        public void logging (string warning){
+            if (!isActive)
+            {
+                isActive = true;
+                Trace.Listeners.Add(new TextWriterTraceListener(path));
+                Trace.AutoFlush = true;
+            }
+            Trace.WriteLine($"{dateTime} warning");
+        }
+    }
+}
