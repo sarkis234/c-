1fded0505a7f15560db0dc35e4e764c2293fd560
sarkis234 <sarkis95283@gmail.com>
2025-09-17 21:24:11 +0300

    Add files via upload

---
+++ 17.09.25/logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+namespace _17._09._25
+{
+    public class logger
+    {
+        public enum errorType
+        {
+            Message,
+            Warning,
+            FatalError
+        }
+        string path = "log.txt";
+        DateTime dateTime = DateTime.Now;
+        bool isActive = false;
+
+        public void logging(string warning, errorType errorType)
+        {
+            if (!isActive)
+            {
+                isActive = true;
+                Trace.Listeners.Add(new TextWriterTraceListener(path));
+                Trace.AutoFlush = true;
+            } 
+            Trace.WriteLine($"{dateTime} {warning}_{errorType}");
+        }
+    }
+}
